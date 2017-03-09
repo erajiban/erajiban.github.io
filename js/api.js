d6fb8131def147cc6eabbbec33cbf59e00f0fd2e
erajiban <emily.rajiban@devinci.fr>
2017-03-09 21:23:34 +0100

    Update api.js

--- js/api.js
+++ js/api.js
@@ -7,7 +7,7 @@ function homePageLoading() {
     fonctionRequeteApi("http://bitcoin.mubiz.com/blockchaininfo", "blockchaininfo");   
 }
 
-/* fonction pour integrer l'API*/
+/* fonction pour integrer l'API
 function fonctionRequeteApi(url,elementID)
 {
     var xmlhttp = new XMLHttpRequest();
@@ -20,4 +20,14 @@ function fonctionRequeteApi(url,elementID)
     };
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
-}
+}*/
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var myObj = this.responseText;
+        var jsonPretty = JSON.stringify(JSON.parse(myObj),null,2);
+        document.getElementById("demo").innerHTML = jsonPretty;
+    }
+};
+xmlhttp.open("GET", "https://block.io/api/v2/get_balance/?api_key=d062-907e-81d9-49d4", true);
+xmlhttp.send();
